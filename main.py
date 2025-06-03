54eb7826bcc8dadc3228dd100285b179f87d598a
rtasker15 <rtasker15@gmail.com>
2025-06-03 21:03:46 +0100

    working on FPS setup, unresolved

--- main.py
+++ main.py
@@ -5,13 +5,17 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    clock = pygame.time.Clock
+    dt = 0
+
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        pygame.time.Clock.tick()
 
 if __name__ == "__main__":
     main()
