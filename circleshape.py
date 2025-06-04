850ae5785211e0ce69a5aaafe521fa7802a76db7
rtasker15 <rtasker15@gmail.com>
2025-06-04 20:14:06 +0100

    successfully added player sprite

--- circleshape.py
+++ circleshape.py
@@ -14,7 +14,7 @@ class CircleShape(pygame.sprite.Sprite):
         self.radius = radius
 
     def draw(self, screen):
-        # sub-classes must override
+        pygame.draw.polygon(screen, "white", self.triangle(), 2)
         pass
 
     def update(self, dt):
--- main.py
+++ main.py
@@ -1,25 +1,27 @@
 import pygame
 
 from constants import *
+from player import *
+from circleshape import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    
+    player_1 = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
 
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        print(dt)
         screen.fill("black")
+        player_1.draw(screen)
         pygame.display.flip()
         clock.tick(60)
         dt = (clock.tick(60) / 1000)
-    
-    print(dt)
 
 if __name__ == "__main__":
     main()
