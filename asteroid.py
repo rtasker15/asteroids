34e743e8e317430a6b79cb04dfbaef6315d523fe
rtasker15 <rtasker15@gmail.com>
2025-06-07 21:51:34 +0100

    Asteroids v1.0 complete

--- asteroid.py
+++ asteroid.py
@@ -4,7 +4,7 @@ from constants import *
 
 class Asteroid(CircleShape):
     def __init__(self, position, velocity, radius):
-        super().__init__(position.x, position.y, radius)
+        super().__init__(position, velocity, radius)
 
         self.velocity = velocity
 
@@ -22,21 +22,10 @@ class Asteroid(CircleShape):
             rand_angle = random.uniform(20, 50)
             new_radius = self.radius - ASTEROID_MIN_RADIUS
 
-            print("self.velocity")
-            print(self.velocity)
-
             new_velocity_1 = pygame.Vector2(self.velocity).rotate(rand_angle) * 1.2
             new_velocity_2 = pygame.Vector2(self.velocity).rotate(-rand_angle) * 1.2
 
-            print("new_velocity_1")
-            print(new_velocity_1)
-            print("new_velocity_2")
-            print(new_velocity_2)
-
             new_asteroid_1 = Asteroid(self.position, new_velocity_1, new_radius)
             new_asteroid_2 = Asteroid(self.position, new_velocity_2, new_radius)
-
-            print("Asteroid object velocity")
-            print(new_asteroid_1.velocity)
 
             self.kill()
